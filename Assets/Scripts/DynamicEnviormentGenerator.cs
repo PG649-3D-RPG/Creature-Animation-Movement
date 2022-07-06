27b239d60149e7eec9569e897193017df92d63ae
Nils <nils.dunker@udo.edu>
2022-07-07 00:00:49 +0200

    Added Size Option

--- Assets/Scripts/DynamicEnviormentGenerator.cs
+++ Assets/Scripts/DynamicEnviormentGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.MLAgents;
@@ -18,11 +19,14 @@ public class DynamicEnviormentGenerator : MonoBehaviour
 
     [SerializeField] public GameObject ObstaclePrefab;
 
+    [Header("Arena Settings")]
+    [SerializeField]
+    public int ArenaCount = 100;
+
+
     [Header("Settings for the terrain")]
     [SerializeField] 
-    public int TerrainXSize = 128;
-    [SerializeField]
-    public int TerrainZSize = 128;
+    public int TerrainSize = 128;
     [SerializeField]
     public int Depth = 10;
     [SerializeField]
@@ -42,7 +46,7 @@ public class DynamicEnviormentGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        var arena = GenerateArena();
+         GenerateArena();
         //GenerateCreature(arena);
         //AddTargetToArena(arena);
     }
@@ -53,45 +57,63 @@ public class DynamicEnviormentGenerator : MonoBehaviour
         
     }
 
-    private GameObject GenerateArena()
+    private void GenerateArena()
     {
-        var arena = new GameObject
+        var posX = (int) Math.Ceiling(Math.Sqrt(ArenaCount));
+        var posZ = posX;
+
+        var posXCounter = 0;
+        var posZCounter = 0;
+
+        var arenaContainer = new GameObject(name = "Arena Container");
+
+        var arena = new GameObject();
+
+        for (var i = 0; i < ArenaCount; i++, posXCounter++)
         {
-            name = "Creature Arena"
-        };
+            Instantiate(arena, new Vector3(posXCounter * TerrainSize, 0, posZCounter * TerrainSize),
+                Quaternion.identity, arenaContainer.transform);
+            arena.name = $"Arena {i}";
 
-        var terrainObj = new GameObject
-        {
-            name = "Terrain",
-            transform = { parent = arena.transform }
-        };
+            var terrainObj = new GameObject
+            {
+                name = $"Terrain {i}",
+                transform = { parent = arena.transform}
+            };
 
 
-        var terrain = terrainObj.AddComponent<Terrain>();
-        var terrainGenerator = terrain.AddComponent<TerrainGenerator>();
-        var colliderObj =  terrain.AddComponent<TerrainCollider>(); 
-        terrain.terrainData = new TerrainData();
-        colliderObj.terrainData = terrain.terrainData;
-        terrainGenerator.width = 128;
-        terrainGenerator.length = 128;
-        terrainGenerator._bakeNavMesh = BakeNavMesh;
-        terrainGenerator._generateHeights = GenerateHeights;
-        terrainGenerator._generateObstacles = GenerateObstacles;
-        terrainGenerator._scaleObstacle = ScaleObstacle;
-        terrainGenerator.depth = Depth;
-        terrainGenerator.scale = Scale;
-        terrainGenerator._obstaclesPrefab = ObstaclePrefab;
-        
-        var wall1 = Instantiate(WallPrefab, new Vector3(64,12,126), Quaternion.identity, terrain.transform);
-        wall1.name = "Wall North";
-        var wall2 = Instantiate(WallPrefab, new Vector3(126,12,64), Quaternion.Euler(0,90,0), terrain.transform);
-        wall2.name = "Wall East";
-        var wall3 = Instantiate(WallPrefab, new Vector3(64,12,2), Quaternion.identity, terrain.transform);
-        wall3.name = "Wall South";
-        var wall4 = Instantiate(WallPrefab, new Vector3(2,12,64), Quaternion.Euler(0,90,0), terrain.transform);
-        wall4.name = "Wall West";
+            var terrain = terrainObj.AddComponent<Terrain>();
+            var terrainGenerator = terrain.AddComponent<TerrainGenerator>();
+            var colliderObj = terrain.AddComponent<TerrainCollider>();
+            terrain.terrainData = new TerrainData();
+            colliderObj.terrainData = terrain.terrainData;
+            terrainGenerator.width = TerrainSize;
+            terrainGenerator.length = TerrainSize;
+            terrainGenerator._bakeNavMesh = BakeNavMesh;
+            terrainGenerator._generateHeights = GenerateHeights;
+            terrainGenerator._generateObstacles = GenerateObstacles;
+            terrainGenerator._scaleObstacle = ScaleObstacle;
+            terrainGenerator.depth = Depth;
+            terrainGenerator.scale = Scale;
+            terrainGenerator._obstaclesPrefab = ObstaclePrefab;
 
-        return arena;
+            var wall1 = Instantiate(WallPrefab, new Vector3(64, 12, 126), Quaternion.identity, terrain.transform);
+            wall1.name = "Wall North";
+            var wall2 = Instantiate(WallPrefab, new Vector3(126, 12, 64), Quaternion.Euler(0, 90, 0), terrain.transform);
+            wall2.name = "Wall East";
+            var wall3 = Instantiate(WallPrefab, new Vector3(64, 12, 2), Quaternion.identity, terrain.transform);
+            wall3.name = "Wall South";
+            var wall4 = Instantiate(WallPrefab, new Vector3(2, 12, 64), Quaternion.Euler(0, 90, 0), terrain.transform);
+            wall4.name = "Wall West";
+
+            if (posXCounter == posX - 1)
+            {
+                posXCounter = -1;
+                posZCounter++;
+            }
+        }
+
+        Destroy(arena);
     }
 
     public void GenerateCreature(GameObject arena)

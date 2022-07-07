2c38592c88e84ff49d97227c761285563a897692
Nils <nils.dunker@udo.edu>
2022-07-07 16:18:41 +0200

    Refactor weil die Methoden zu lang wurden

--- Assets/Scripts/DynamicEnviormentGenerator.cs
+++ Assets/Scripts/DynamicEnviormentGenerator.cs
@@ -26,7 +26,6 @@ public class DynamicEnviormentGenerator : MonoBehaviour
     [SerializeField]
     public int ArenaCount = 10;
 
-
     [Header("Terrain settings")]
     [SerializeField] 
     public int TerrainSize = 128;
@@ -48,39 +47,30 @@ public class DynamicEnviormentGenerator : MonoBehaviour
     [Header("Creature Settings")]
     [SerializeField]
     public bool regenerateTerrain = true;
-
     [SerializeField]
     public int regenerateTerrainAfterXSteps = 1;
-
     [SerializeField]
     public bool placeTargetCubeRandomly = true;
-
     [SerializeField]
     public int placeTargetCubeRandomlyAfterXSteps = 1;
-
     [SerializeField]
     public bool fastResetForTheFirstEpisodes = true;
-
     [SerializeField]
     public int fastResetLength = 10000000;
-
-    //[SerializeField]
-    //public List<BoneCategory> notAllowedToTouchGround = new() { BoneCategory.Head };
-
-    //[SerializeField]
-    //public List<BoneCategory> notAllowedToTouchGroundInFastPhase = new() { BoneCategory.Arm, BoneCategory.Hand, BoneCategory.Torso };
-
+    [SerializeField]
+    public List<BoneCategory> notAllowedToTouchGround = new() { BoneCategory.Head };
+    [SerializeField]
+    public List<BoneCategory> notAllowedToTouchGroundInFastPhase = new() { BoneCategory.Arm, BoneCategory.Hand, BoneCategory.Torso };
     [SerializeField] 
     public bool penalizeGroundContact = true;
-
-    //[SerializeField]
-    //private FlexibleDictionary<BoneCategory, int> penaltiesForBodyParts = new() {{BoneCategory.Arm, 2}, {BoneCategory.Hand, 5},
-    //    {BoneCategory.Head, 10}, {BoneCategory.Hip, 5}, {BoneCategory.Leg, 1}, {BoneCategory.Shoulder, 5}};
-    // Start is called before the first frame update
+    [SerializeField]
+    private FlexibleDictionary<BoneCategory, int> penaltiesForBodyParts = new() {{BoneCategory.Arm, 2}, {BoneCategory.Hand, 5},
+        {BoneCategory.Head, 10}, {BoneCategory.Hip, 5}, {BoneCategory.Leg, 1}, {BoneCategory.Shoulder, 5}};
     
+    // Start is called before the first frame update
     void Awake()
     {
-        GenerateArena();
+        GenerateTrainingEnvironment();
 
         //GenerateCreature(arena);
         //AddTargetToArena(arena);
@@ -96,9 +86,10 @@ public class DynamicEnviormentGenerator : MonoBehaviour
     {
     }
 
-    private void GenerateArena()
+
+    public void GenerateTrainingEnvironment()
     {
-        var posX = (int) Math.Ceiling(Math.Sqrt(ArenaCount));
+        var posX = (int)Math.Ceiling(Math.Sqrt(ArenaCount));
 
         var posXCounter = 0;
         var posZCounter = 0;
@@ -110,62 +101,67 @@ public class DynamicEnviormentGenerator : MonoBehaviour
 
         for (var i = 0; i < ArenaCount; i++, posXCounter++)
         {
-            var arena = new GameObject
-            {
-                name = $"Arena {i}",
-                transform =
-                {
-                    parent = arenaContainer.transform,
-                    localPosition = new Vector3(posXCounter * TerrainSize, 0, posZCounter * TerrainSize )
-                }
-            };
-
-            var terrainObj = new GameObject
-            {
-                name = $"Terrain {i}",
-                transform =
-                {
-                    parent = arena.transform,
-                    localPosition = Vector3.zero
-                }
-            };
-
-
-            var terrain = terrainObj.AddComponent<Terrain>();
-            var terrainGenerator = terrain.AddComponent<TerrainGenerator>();
-            var colliderObj = terrain.AddComponent<TerrainCollider>();
-            terrain.terrainData = new TerrainData();
-            colliderObj.terrainData = terrain.terrainData;
-            terrainGenerator.width = TerrainSize;
-            terrainGenerator.length = TerrainSize;
-            terrainGenerator._bakeNavMesh = BakeNavMesh;
-            terrainGenerator._generateHeights = GenerateHeights;
-            terrainGenerator._generateObstacles = GenerateObstacles;
-            terrainGenerator._scaleObstacle = ScaleObstacle;
-            terrainGenerator.depth = Depth;
-            terrainGenerator.scale = Scale;
-            terrainGenerator._obstaclesPrefab = ObstaclePrefab;
-
-            var wall1 = Instantiate(WallPrefab, new Vector3(64, 12, 126), Quaternion.identity, terrain.transform);
-            wall1.name = "Wall North";
-            wall1.transform.localPosition = new Vector3(64, 12, 126);
-            var wall2 = Instantiate(WallPrefab, new Vector3(126, 12, 64), Quaternion.Euler(0, 90, 0), terrain.transform);
-            wall2.name = "Wall East";
-            wall2.transform.localPosition = new Vector3(126, 12, 64);
-            var wall3 = Instantiate(WallPrefab, new Vector3(64, 12, 2), Quaternion.identity, terrain.transform);
-            wall3.name = "Wall South";
-            wall3.transform.localPosition = new Vector3(64, 12, 2);
-            var wall4 = Instantiate(WallPrefab, new Vector3(2, 12, 64), Quaternion.Euler(0, 90, 0), terrain.transform);
-            wall4.name = "Wall West";
-            wall4.transform.localPosition = new Vector3(2, 12, 64);
-
-
+            var arena = GenerateArena(i, posXCounter, posZCounter, posX, arenaContainer);
             if (posXCounter == posX - 1)
             {
                 posXCounter = -1;
                 posZCounter++;
             }
         }
+    }
+
+    private GameObject GenerateArena(int i, int posXCounter, int posZCounter, int posX, GameObject arenaContainer)
+    {
+        var arena = new GameObject
+        {
+            name = $"Arena {i}",
+            transform =
+                {
+                    parent = arenaContainer.transform,
+                    localPosition = new Vector3(posXCounter * TerrainSize, 0, posZCounter * TerrainSize )
+                }
+        };
+
+        var terrainObj = new GameObject
+        {
+            name = $"Terrain {i}",
+            transform =
+                {
+                    parent = arena.transform,
+                    localPosition = Vector3.zero
+                }
+        };
+
+
+        var terrain = terrainObj.AddComponent<Terrain>();
+        var terrainGenerator = terrain.AddComponent<TerrainGenerator>();
+        var colliderObj = terrain.AddComponent<TerrainCollider>();
+        terrain.terrainData = new TerrainData();
+        colliderObj.terrainData = terrain.terrainData;
+        terrainGenerator.width = TerrainSize;
+        terrainGenerator.length = TerrainSize;
+        terrainGenerator._bakeNavMesh = BakeNavMesh;
+        terrainGenerator._generateHeights = GenerateHeights;
+        terrainGenerator._generateObstacles = GenerateObstacles;
+        terrainGenerator._scaleObstacle = ScaleObstacle;
+        terrainGenerator.depth = Depth;
+        terrainGenerator.scale = Scale;
+        terrainGenerator._obstaclesPrefab = ObstaclePrefab;
+
+        var wall1 = Instantiate(WallPrefab, new Vector3(64, 12, 126), Quaternion.identity, terrain.transform);
+        wall1.name = "Wall North";
+        wall1.transform.localPosition = new Vector3(64, 12, 126);
+        var wall2 = Instantiate(WallPrefab, new Vector3(126, 12, 64), Quaternion.Euler(0, 90, 0), terrain.transform);
+        wall2.name = "Wall East";
+        wall2.transform.localPosition = new Vector3(126, 12, 64);
+        var wall3 = Instantiate(WallPrefab, new Vector3(64, 12, 2), Quaternion.identity, terrain.transform);
+        wall3.name = "Wall South";
+        wall3.transform.localPosition = new Vector3(64, 12, 2);
+        var wall4 = Instantiate(WallPrefab, new Vector3(2, 12, 64), Quaternion.Euler(0, 90, 0), terrain.transform);
+        wall4.name = "Wall West";
+        wall4.transform.localPosition = new Vector3(2, 12, 64);
+
+        return arena;
     }
 
     public void GenerateCreature(GameObject arena)

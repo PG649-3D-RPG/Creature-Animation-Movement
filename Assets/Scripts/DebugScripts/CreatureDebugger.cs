7c9ade0fcb455edabc145e01f2875bdbdcd736a0
nils <nils.dunker@tu-dortmund.de>
2022-10-17 13:39:30 +0200

    Improved version of our creature debugger

--- Assets/Scripts/DebugScripts/CreatureDebugger.cs
+++ Assets/Scripts/DebugScripts/CreatureDebugger.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class CreatureDebugger : MonoBehaviour
 {
@@ -9,11 +11,40 @@ public class CreatureDebugger : MonoBehaviour
     [SerializeField] public int Seed = 0;
     [SerializeField] public ScriptableObject CreatureGeneratorSettings;
     [SerializeField] public ScriptableObject ParametricCreatureSettings;
+    [SerializeField] public bool DisablePhysics;
     [SerializeField] public CreatureType CreatureType;
+    
 
     private GameObject creature;
     private GameObject debugCreature;
 
+
+    private void Start()
+    {
+        if(CreatureGeneratorSettings is null || ParametricCreatureSettings is null) Debug.LogError("No config object for creature.");
+        if(DisablePhysics) Physics.autoSimulation = false;
+        else CreateDummyTerrain();
+    }
+
+    private void CreateDummyTerrain()
+    {
+        var terrainObj = new GameObject
+        {
+            name = $"Terrain",
+            transform =
+            {
+                localPosition = new Vector3(-50, -1, -50)
+            }
+        };
+        var terrain = terrainObj.AddComponent<Terrain>();
+        terrain.terrainData = new TerrainData();
+        var colliderObj = terrain.AddComponent<TerrainCollider>();
+        colliderObj.terrainData = terrain.terrainData;
+        colliderObj.terrainData.size = new Vector3(250, 0, 250);
+        terrain.materialTemplate = Resources.Load("GridMatFloor", typeof(Material)) as Material;
+
+        
+    }
     // Update is called once per frame
     void Update()
     {
@@ -36,8 +67,16 @@ public class CreatureDebugger : MonoBehaviour
     private void GenCreature()
     {
         Destroy(debugCreature);
-        if(CreatureType == CreatureType.Biped) creature = CreatureGenerator.ParametricBiped((CreatureGeneratorSettings)CreatureGeneratorSettings, (ParametricCreatureSettings)ParametricCreatureSettings, Seed);
-        if(CreatureType == CreatureType.Quadruped) creature = CreatureGenerator.ParametricQuadruped((CreatureGeneratorSettings)CreatureGeneratorSettings, (ParametricCreatureSettings)ParametricCreatureSettings, Seed);
+        creature = CreatureType switch
+        {
+            CreatureType.Biped => CreatureGenerator.ParametricBiped(
+                (CreatureGeneratorSettings)CreatureGeneratorSettings,
+                (ParametricCreatureSettings)ParametricCreatureSettings, Seed),
+            CreatureType.Quadruped => CreatureGenerator.ParametricQuadruped(
+                (CreatureGeneratorSettings)CreatureGeneratorSettings,
+                (ParametricCreatureSettings)ParametricCreatureSettings, Seed),
+            _ => creature
+        };
 
         debugCreature = new GameObject
         {

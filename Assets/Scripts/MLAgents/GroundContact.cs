3c7a292a32e1e737c877eff3194802b5533c9e2b
nils <nils.dunker@tu-dortmund.de>
2023-01-20 20:12:16 +0100

    Implement messy wall detection

--- Assets/Scripts/MLAgents/GroundContact.cs
+++ Assets/Scripts/MLAgents/GroundContact.cs
@@ -19,9 +19,11 @@ namespace Unity.MLAgentsExamples
     {
         public Agent Agent;
         public bool TouchingGround;
+        public bool TouchingWall;
         private DynamicEnvironmentGenerator _deg;
         private Bone _boneScript;
         private CreatureConfig _creatureConfig;
+        private Terrain _terrain;
         private const string GroundTag = "ground";
 
         public GroundContact(Agent agent)
@@ -33,7 +35,7 @@ namespace Unity.MLAgentsExamples
         {
             _deg = FindObjectOfType<DynamicEnvironmentGenerator>();
             _creatureConfig = FindObjectOfType<CreatureConfig>();
-
+            _terrain = FindObjectOfType<Terrain>();
             _boneScript = GetComponentInParent<Bone>();
         }
 
@@ -44,28 +46,44 @@ namespace Unity.MLAgentsExamples
         {
             if (col.transform.CompareTag(GroundTag))
             {
-                TouchingGround = true;
-                switch (_boneScript.category)
+                var isWall = false;
+                var contacts = new ContactPoint[col.contactCount];
+                col.GetContacts(contacts);
+                
+                foreach (var pos in contacts)
                 {
-                    case BoneCategory.Leg:
-                    case BoneCategory.Arm:
-                    case BoneCategory.Torso:
-                    case BoneCategory.Head:
-                    case BoneCategory.Hand:
-                    case BoneCategory.Shoulder:
-                    case BoneCategory.Hip:
-                        Agent.SetReward(_creatureConfig.ContactPenalty);
-                        break;
-                }
-                if (_creatureConfig.ResetOnGroundContactParts.Contains(_boneScript.category))
-                {
-
-                    Agent.EndEpisode();
-                    if(Application.isEditor) Debug.LogError($"{_boneScript.transform.name} touched the ground");
-
+                    if (_terrain.SampleHeight(pos.point) > 0.3){
+                        if(Application.isEditor) Debug.LogError($"{_boneScript.transform.name} touched the wall");
+                        isWall = true;
+                    }
                 }
 
+                if (!isWall)
+                {
+                    TouchingGround = true;
+                    switch (_boneScript.category)
+                    {
+                        case BoneCategory.Leg:
+                        case BoneCategory.Arm:
+                        case BoneCategory.Torso:
+                        case BoneCategory.Head:
+                        case BoneCategory.Hand:
+                        case BoneCategory.Shoulder:
+                        case BoneCategory.Hip:
+                            Agent.SetReward(_creatureConfig.ContactPenalty);
+                            break;
+                    }
+                    if (_creatureConfig.ResetOnGroundContactParts.Contains(_boneScript.category))
+                    {
+
+                        Agent.EndEpisode();
+                        if(Application.isEditor) Debug.LogError($"{_boneScript.transform.name} touched the ground");
+
+                    }
+                }
             }
+
+            TouchingWall = true;
         }
 
         /// <summary>
@@ -73,7 +91,11 @@ namespace Unity.MLAgentsExamples
         /// </summary>
         private void OnCollisionExit(Collision other)
         {
-            if (other.transform.CompareTag(GroundTag)) TouchingGround = false;
+            if (other.transform.CompareTag(GroundTag))
+            {
+                TouchingGround = false;
+                TouchingWall = false;
+            }
         }
     }
 }
